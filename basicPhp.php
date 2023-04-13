8584695243edb5a153275f5574c6ffc2ef2696b8
taralamia <79480117+taralamia@users.noreply.github.com>
2023-04-14 00:26:05 +0600

    variable,concatenation,new line print learnt

---
+++ basicPhp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>
+<?php
+echo "Hello,I'm tabassum.";
+$firstName = "Joe";
+$age = 10;
+
+echo "<br>";
+echo "$firstName";
+echo "First Name ". $firstName ," Age ". $age; //had to use dot while using any variable need to use space also
+echo "<br>";
+echo "The " . "Book " . "Name " . "is " . "It ends with us!";
+
+?>
+</h2>
+
+
+</body>
+</html>
