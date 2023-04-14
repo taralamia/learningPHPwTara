6aaa5db8b6f67aa2841f805b919896d47a81dca0
taralamia <79480117+taralamia@users.noreply.github.com>
2023-04-15 02:12:10 +0600

    sum of two numbers

---
+++ sumOfTwoNumber.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+    <?php
+    $a = 2;
+    $b = 3;
+    $output = $a + $b;
+    echo "The sum of two operand is ". ($a + $b);
+    ?>
+</h1>
+
+</body>
+</html>
