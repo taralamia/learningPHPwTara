0226e43da0be63986604b095b963a8a13fe9fbbd
taralamia <79480117+taralamia@users.noreply.github.com>
2023-04-15 02:24:16 +0600

    ALU performed through PHP

---
+++ allArithmeticOperation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+    <?php
+    $a = 2;
+    $b = 2;
+    echo "The sum of a and b is ". ($a + $b);
+    echo "<br>";
+    echo "The difference of a and b is " . ($a - $b);
+    echo "<br>";
+    echo "The product of a and b is " . ($a * $b);
+    echo "<br>";
+    echo "The quotient of a and b is " . ($a / $b);
+    ?>
+
+   </h1>
+
+</body>
+</html>
