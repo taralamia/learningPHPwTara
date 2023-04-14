f075a840c86cb098ffdba9df315602b2421114a5
taralamia <79480117+taralamia@users.noreply.github.com>
2023-04-15 03:16:19 +0600

    circle measurement

---
+++ circle.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+    <?php
+    $radius = 1.5;
+    echo "The area of the circle is "  . (3.1416 * $radius * $radius);
+    echo "<br>";
+    echo "The circumference of the circle is " . (2 * 3.1416 * $radius);
+    echo "<br>";
+    echo "The diameter of the circle is " . (2 * $radius);
+
+    ?>
+</h1>
+
+</body>
+</html>
