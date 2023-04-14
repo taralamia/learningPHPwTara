069875b35d9b23e459e9114602edec6df308888b
taralamia <79480117+taralamia@users.noreply.github.com>
2023-04-15 02:32:46 +0600

    rectangle area and perimeter

---
+++ rectangle.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+    <?php
+    $h = 3;
+    $w = 2;
+    echo "The area of the rectangle is " . ($h * $w);
+    echo "<br>";
+    echo "The perimeter of the rectangle is " . (2 * ($h + $w));
+    ?>
+</h1>
+
+</body>
+</html>
